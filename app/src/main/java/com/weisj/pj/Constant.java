d1e2953136dd86c55753ac65e74334fba9317b8f
June <juner122king@gmail.com>
2018-01-19 17:33:26 +0800

    完成品类逻辑-》完成个人中心，收货地址
    购卡支付成功，成功接收微信的回调

---
+++ app/src/main/java/com/weisj/pj/Constant.java
@@ -0,0 +1,29 @@
+package com.weisj.pj;
+
+
+public class Constant {
+//	//微信公众平台id;
+
+	public static String app_wx_appid="wx3d41218648b9bdac";
+
+
+//	//微信开放平台和商户约定的密钥
+	public static String app_wx_secret_key="c5224e105de711e914103de48e9924a8";
+//	//微信公众平台商户模块和商户约定的密钥
+	public static String app_wx_parent_key="62d1a04a657e7da758a1456cf2455ae5";
+//	//微信公众平台商户模块和商户约定的支付密钥
+	public static String app_wx_pay_key="HDguC0MwkUd4wPaEtX22g76AgWhi76qtZKOiPZlI0eS5UIgqkFNoQ0lSydmqAFmrms6HAzf1x7RjwzptWKXtqHdCqq0j76r6dpVzZ6eIzmK8AOpMnABRwf9rzz2ihseH";
+//	// 商家向财付通申请的商家id */
+	public static String partner_id = "1224583001";
+	
+	public static String successCode = "0#1#1";
+	
+	//支付宝
+	public static final String partner = "2088711172300495";
+	
+	public static final String rsa_private = 
+			"MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAJKcEigIhPABHVuix+AGHt4idZKsjnZp5qOP7C4k6wn4YCg7AYJskXOaIpY5Jj0rXkBo+YAShjUOPWEGKV2/mzH0z8Qglzx3AAS7H3UwtAPjFspuWotXekIvq32OAhnNx/Zs0S23XDUtmTJYMOU/1a8cBOfGLMYoGpyqv0egoN9rAgMBAAECgYACLBPMyguGLs4LjIbfl9efPO13Bd16fakwJTK/oEZnOGncTrt//jHNf6whg6lAYMMduzBvKFcatCJOrLOyMuOOvS8vHV0mK944gOqh8qSAjpvxlQOZK/DltoSPyb/nKEdeRmxtJiZRmn/jzu7Tx9SaMLVhk3z+i5DYJgcASTZe+QJBAMH5OH13mJI0WX47J3yYxu6RbgVDwzwrpQHLDeQvHEnXnbLAUIxit/fYLw77h+Xuk5wqVgaHHn1/kgfqX3ZsEf0CQQDBfZ42Kxl5puyV99lc/BNFEIP37/oOokfphSJVRn3c6ZwC6SqHIJLgd1/fUaZ4pftAsRoWf/Hd6fJqG763WN+HAkEAr95ilX+F2amVKpo/pvxuAYfiFOvna/q2VPXh1m3QXDMeTB+J6H7pBdIAeLkd8h9pvkG0VZ7UyjyMDpk1jI1XkQJAYhcUxdFDsPB2r/BeOU1xV2nv/RmjYxfR7QiZRzHdTMkBVlhuPuK8kx7xc30pL7C1ly4xU4a5Tw7y3egC32AmsQJAT1LD82FG0lpWe8v/Ogpo7XsNKiaJGOvXLQf4i769wzav/bGKYR6rIChiFkhaWlxYfANHd76Nv31rLz+kcfgc6A==";
+
+	public static final String seller = "szygbh@qq.com";
+	
+}
